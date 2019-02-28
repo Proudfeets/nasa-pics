be8bc86902f5ca7e8d89b1cb55c60aa873333e53
PROUDFEETS <sgabisouza@gmail.com>
2019-02-28 17:44:03 -0500

    Basic set up with fetch request, component, and tile.

---
+++ src/containers/PhotoContainer.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import PhotoTile from "../components/PhotoTile";
+
+class PhotoContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clickedOn: true
+    };
+  }
+
+componentDidMount(){
+    fetch(`https://api.nasa.gov/EPIC/api/natural/images?api_key=KeyGoesHere`)
+    .then((response) => {
+        let responseBody = response.json()
+        return responseBody;
+    })
+    .then(responseBody => {
+      this.setState({
+        photos:responseBody.photos
+        });
+      });
+    }
+
+  render(){
+
+    return(
+      <div>
+      <h3>"Photo Container!"</h3>
+    <PhotoTile/>
+    </div>
+    )
+  }
+}
+
+export default PhotoContainer;
